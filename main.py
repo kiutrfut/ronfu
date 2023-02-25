b48f5d7cd082b896e223773e6ebca826fe936844
kiutrfut <99256842+kiutrfut@users.noreply.github.com>
2023-02-26 01:50:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,64 +1,44 @@
 import os
-import time
-import shutil
-from hurry.filesize import size
+import subprocess
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from status import get_status_message
 
-# Create a new Telegram Bot using the Bot Father and get the API token
-API_TOKEN = "5959482663:AAGnBMV2Rbrtr5k01AxYXrw-bRSJ9mIEjwk"
+from status import get_file_status, convert_to_streamable_video
 
-# Create a new Pyrogram client instance
-app = Client("my_bot", api_id=7068313, api_hash="d7446aca34e84b8539a1a8817630d1b5", bot_token=API_TOKEN)
 
-# Define the "/start" command handler
-@app.on_message(filters.command("start"))
-def start_command_handler(client: Client, message: Message):
-    # Send a welcome message to the user
-    message.reply_text("Hello! I'm a Telegram Bot. Send me a file and type /convert to convert it into a streamable video format.")
+# Environment variables
+API_ID = os.environ.get("7068313")
+API_HASH = os.environ.get("d7446aca34e84b8539a1a8817630d1b5")
+BOT_TOKEN = os.environ.get("5959482663:AAGnBMV2Rbrtr5k01AxYXrw-bRSJ9mIEjwk")
 
-# Define the "/convert" command handler
-@app.on_message(filters.command("convert"))
-def convert_command_handler(client: Client, message: Message):
-    # Check if the message contains a file
-    if not message.document:
-        message.reply_text("Please send a file to convert.")
-        return
 
-    # Check if the file is in video format
-    if not message.document.mime_type.startswith("video/"):
-        message.reply_text("Sorry, I can only convert video files.")
-        return
+# Create a Pyrogram client instance
+app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-    # Download the file
-    message.reply_text("Downloading file...")
-    file_path = f"{message.document.file_name}_{message.document.file_id}"
-    file = client.download_media(message=message, file_name=file_path)
 
-    # Convert the file
-    message.reply_text("Converting file...")
-    output_file_path = f"{os.path.splitext(file_path)[0]}.mp4"
-    os.system(f"ffmpeg -i '{file}' -codec:v libx264 -profile:v main -preset slow -b:v 500k -maxrate 500k -bufsize 1000k -vf scale=-2:720 -threads 0 -codec:a libfdk_aac -b:a 128k '{output_file_path}'")
-
-    # Delete the original file
-    os.remove(file)
-
-    # Send the converted file
-    message.reply_text("Sending file...")
-    with open(output_file_path, "rb") as f:
-        app.send_video(chat_id=message.chat.id, video=f, caption=f"Converted file ({size(os.path.getsize(output_file_path))})")
-
-    # Delete the converted file
-    os.remove(output_file_path)
-
-# Define the "/status" command handler
+# Command to check the status of a file
 @app.on_message(filters.command("status"))
 def status_command_handler(client: Client, message: Message):
-    # Call the get_status_message function from the status.py file
-    # to get the current status of file downloading and uploading
-    status_msg = get_status_message()
+    # Get the file status
+    file_path = message.reply_to_message.document.file_name
+    status_msg = get_file_status(file_path)
+    
+    # Reply with the status message
     message.reply_text(status_msg)
 
-# Start the Bot
+
+# Command to convert a file to streamable video format
+@app.on_message(filters.command("convert"))
+def convert_command_handler(client: Client, message: Message):
+    # Download the file and get its path
+    file_path = client.download_media(message.reply_to_message)
+    
+    # Convert the file to streamable video format
+    converted_file_path = convert_to_streamable_video(file_path)
+    
+    # Reply with the converted file
+    message.reply_document(document=converted_file_path)
+    
+    
+# Run the client
 app.run()
