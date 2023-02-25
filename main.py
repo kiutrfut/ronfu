a2c4fdbb093e3f0143028a99581437ef9a1b9ba7
kiutrfut <99256842+kiutrfut@users.noreply.github.com>
2023-02-26 03:18:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,74 @@
-import os
 import subprocess
-
+from pyrogram import types
+import os
+import logging
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from status import get_file_status, get_video_info, get_file_size, convert_to_streamable_video
+from status import get_file_status, get_video_file_id, convert_to_streamable_video
 
-app = Client("my_bot")
 
-# Status command handler
+# Get the environment variables
+API_ID = int(os.environ.get("API_ID", 7068313))
+API_HASH = os.environ.get("API_HASH", "d7446aca34e84b8539a1a8817630d1b5")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5959482663:AAGnBMV2Rbrtr5k01AxYXrw-bRSJ9mIEjwk")
+
+if API_ID == 0 or not API_HASH or not BOT_TOKEN:
+    print("Please provide API_ID, API_HASH and BOT_TOKEN as environment variables.")
+    quit()
+
+# Create the Client and connect to Telegram
+app = Client("ronfu_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("RonfuBot")
+
+
+# Define the /start command handler
+@app.on_message(filters.command(["start"]))
+def start_command_handler(_, message: Message) -> None:
+    message.reply_text("Hi, send me a file and I will tell you its details!")
+
+
+# Define the /status command handler
 @app.on_message(filters.command(["status"]))
-def status_command_handler(client: Client, message: Message):
-    if message.reply_to_message:
-        file_id = message.reply_to_message.video.file_id
-        status_msg = get_file_status(client, message.chat.id, file_id)
-        message.reply_text(status_msg)
-    else:
-        message.reply_text("Please reply to a video file to get its status.")
+def status_command_handler(_, message: Message) -> None:
+    file_id = get_video_file_id(message.reply_to_message) if message.reply_to_message else None
+    if not file_id:
+        message.reply_text("Please reply to a video file or stream to get its status.")
+        return
+
+    status_msg = get_file_status(app, message.chat.id, file_id)
+    message.reply_text(status_msg)
 
 
-# Convert command handler
+# Define the /convert command handler
 @app.on_message(filters.command(["convert"]))
-def convert_command_handler(client: Client, message: Message):
-    if message.reply_to_message:
-        # Get file info
-        file_id = message.reply_to_message.video.file_id
-        file_name = message.reply_to_message.video.file_name
-        file_size = get_file_size(client, file_id)
-        duration = get_video_info(client, file_id)
+def convert_command_handler(_, message: Message) -> None:
+    file_id = get_video_file_id(message.reply_to_message) if message.reply_to_message else None
+    if not file_id:
+        message.reply_text("Please reply to a video file or stream to convert it.")
+        return
 
-        # Send initial message
-        initial_msg = f"Converting to streamable format...\n\n<b>File name:</b> {file_name}\n<b>File size:</b> {file_size}\n<b>Duration:</b> {duration}"
-        message.reply_text(initial_msg, parse_mode="html")
+    file_name = message.reply_to_message.document.file_name
+    chat_id = message.chat.id
+    file_path = app.download_media(file_id)
+    streamable_path = convert_to_streamable_video(chat_id, file_id, file_name, file_path)
 
-        # Convert to streamable format
-        streamable_path = convert_to_streamable_video(client, message.chat.id, file_id, file_name)
-
-        # Upload streamable video
-        message.reply_video(streamable_path, supports_streaming=True)
-
-        # Clean up
-        os.remove(streamable_path)
-    else:
-        message.reply_text("Please reply to a video file to convert it.")
+    message.reply_video(video=streamable_path)
 
 
-app.run()
+def convert_to_streamable_video(chat_id: int, file_id: str, file_name: str, file_path: str) -> str:
+    # Use ffprobe to get the video duration
+    duration = subprocess.check_output(['ffprobe', '-i', file_path, '-show_entries', 'format=duration', '-v', 'quiet', '-of', 'csv=%s' % ("p=0")])
+    duration = int(float(duration))
+
+    # Set the output file name and path
+    streamable_file_name = f"{file_name}_streamable.mp4"
+    streamable_file_path = f"streamable/{chat_id}/{file_id}/{streamable_file_name}"
+
+    # Create the streamable directory if it doesn't exist
+    os.makedirs(os.path.dirname(streamable_file_path), exist_ok=True)
+
+    # Convert the video to a streamable format using ffmpeg
+    subprocess.run(['ffmpeg', '-y', '-i', file_path, '-c:v', 'libx264', '-preset',
